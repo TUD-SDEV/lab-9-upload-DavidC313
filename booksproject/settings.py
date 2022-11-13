052812344e3fdeca16d2f123342622e8ca580232
David <X00181967@mytudublin.ie>
2022-11-13 19:55:56 +0000

    lab 9 part 4 commit

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -150,3 +150,6 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
